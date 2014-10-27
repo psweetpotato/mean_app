d6cf72df45bdb602030404d0fbd002be4c2410de
psweetpotato <p.sweetpotato@gmail.com>
2014-10-27 15:07:31 -0400

    updates bestctrl

--- public/js/controllers/BestCtrl.js
+++ public/js/controllers/BestCtrl.js
@@ -1,5 +1,10 @@
-angular.module('BestCtrl', []).controller('BestController', function($scope) {
-
-    $scope.tagline = 'To the moon and back!';
-
+angular.module('BestCtrl', []).controller('BestController', ['$scope', '$http', 'Best',
+  function($scope, $http, Best) {
+    this.myBestForm = {};
+    console.log(this.myBestForm);
+    this.myBestForm.submitTheBestForm = function($http, bestData)
+      console.log($scope.best.myBestForm);
+      var bestData = $scope.best.myBestForm;
+      console.log(bestData);
+      Best.create(bestData);
 });
