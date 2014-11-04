6499880c8c1b28700cf1abb4bdaf65f71d77edf9
psweetpotato <p.sweetpotato@gmail.com>
2014-11-04 14:32:41 -0500

    changes redirect to map

--- app/routes.js
+++ app/routes.js
@@ -121,7 +121,7 @@ module.exports = function(app, passport) {
 
     // process the login form
       app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/map', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
@@ -137,7 +137,7 @@ module.exports = function(app, passport) {
 
    // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
+        successRedirect : '/map', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
