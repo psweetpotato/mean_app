b299c530ef93d06f3e1986fc1d53c64daf8b0951
psweetpotato <p.sweetpotato@gmail.com>
2014-11-10 15:08:03 -0500

    fixes overlay div for adding

--- public/js/controllers/LayerCtrl.js
+++ public/js/controllers/LayerCtrl.js
@@ -19,6 +19,10 @@ angular.module('LayerCtrl', [])
 
     $scope.visible = false;
 
+    $scope.toggle = function() {
+      $scope.visible = !$scope.visible;
+    };
+
     var addAll = function(){
       $.get('/api/bests',  function(req, res) {
         console.log(req);
@@ -47,31 +51,31 @@ angular.module('LayerCtrl', [])
       categoryLayer.clearLayers(map);
       addAll();
     });
-
-    $(".overlay").on('click', '#cat', function(){
-      categoryLayer.clearLayers(map);
-      var catText = this.innerText;
-      console.log(catText);
-      $.get('/api/bests',  function(req, res) {
-        for (var i = 0; i < req.length; i++) {
-          if (req[i].category == catText) {
-            var venue = req[i].name;
-            var latlng = L.latLng(req[i].lat, req[i].lon);
-            var address = req[i].address;
-            var marker = L.marker(latlng, {
-              icon: L.mapbox.marker.icon({
-                'marker-color': '#F9AC6D',
-                'marker-symbol': 'restaurant',
-                'marker-size': 'medium'
+    if ($scope.visible === true) {
+      $(".overlay").on('click', '.cat', function(){
+        categoryLayer.clearLayers(map);
+        var catText = this.innerText;
+        console.log(catText);
+        $.get('/api/bests',  function(req, res) {
+          for (var i = 0; i < req.length; i++) {
+            if (req[i].category == catText) {
+              var venue = req[i].name;
+              var latlng = L.latLng(req[i].lat, req[i].lon);
+              var address = req[i].address;
+              var marker = L.marker(latlng, {
+                icon: L.mapbox.marker.icon({
+                  'marker-color': '#F9AC6D',
+                  'marker-symbol': 'restaurant',
+                  'marker-size': 'medium'
+                })
               })
-            })
-              .bindPopup(venue +'<br/>' + address +"<br/>")
-                .addTo(categoryLayer);
+                .bindPopup(venue +'<br/>' + address +"<br/>")
+                  .addTo(categoryLayer);
+            }
           }
-        }
+        });
       });
-    });
-
+    }
   $(".overlay").on('click', '#mine', function(){
       categoryLayer.clearLayers(map);
       var userid = $('#userId').text();
