40ce90854f86f7224b3354c438580d15c0e1fee9
psweetpotato <p.sweetpotato@gmail.com>
2014-12-02 14:50:03 -0500

    updates user schema for unique emails only

--- app/models/models.js
+++ app/models/models.js
@@ -6,11 +6,24 @@ var Schema = mongoose.Schema,
 var userSchema = new Schema({
   local: {
   id: ObjectId,
-  email: String,
+  email: {type: String, unique: true},
   password: String,
   following: [{type: mongoose.Schema.ObjectId, ref: 'User'}],
   followers: [{type: mongoose.Schema.ObjectId, ref: 'User'}],
   bests: [{type: Number, ref: 'Best'}]
+  // bests: [
+  //     {Bagels: {type: String, ref: 'Best'}},
+  //     {Brunch: {type: String, ref: 'Best'}},
+  //     {Burger: {type: String, ref: 'Best'}},
+  //     {Coffee: {type: String, ref: 'Best'}},
+  //     {Dessert: {type: String, ref: 'Best'}},
+  //     {Doughnuts: {type: String, ref: 'Best'}},
+  //     {IceCream: {type: String, ref: 'Best'}},
+  //     {Pizza: {type: String, ref: 'Best'}},
+  //     {Ramen: {type: String, ref: 'Best'}},
+  //     {Sushi: {type: String, ref: 'Best'}},
+  //     {Tacos: {type: String, ref: 'Best'}}
+  // ]
   }
 });
 
@@ -21,7 +34,7 @@ var bestSchema = mongoose.Schema({
   address: String,
   category: String,
   venue_id: String,
-  user : [{ type: mongoose.Schema.ObjectId, ref: 'User' }]
+  user : [{ type: mongoose.Schema.ObjectId, ref: 'User'}]
 });
 
 userSchema.methods.generateHash = function(password) {
