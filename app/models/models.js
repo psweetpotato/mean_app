069bba013033a76665bbb68d1b0cac56c080d488
psweetpotato <p.sweetpotato@gmail.com>
2014-12-02 14:54:33 -0500

    updates user schema to require email & password

--- app/models/models.js
+++ app/models/models.js
@@ -6,24 +6,24 @@ var Schema = mongoose.Schema,
 var userSchema = new Schema({
   local: {
   id: ObjectId,
-  email: {type: String, unique: true},
-  password: String,
+  email: {type: String, unique: true, required: true},
+  password: {type: String, required: true},
   following: [{type: mongoose.Schema.ObjectId, ref: 'User'}],
   followers: [{type: mongoose.Schema.ObjectId, ref: 'User'}],
-  bests: [{type: Number, ref: 'Best'}]
-  // bests: [
-  //     {Bagels: {type: String, ref: 'Best'}},
-  //     {Brunch: {type: String, ref: 'Best'}},
-  //     {Burger: {type: String, ref: 'Best'}},
-  //     {Coffee: {type: String, ref: 'Best'}},
-  //     {Dessert: {type: String, ref: 'Best'}},
-  //     {Doughnuts: {type: String, ref: 'Best'}},
-  //     {IceCream: {type: String, ref: 'Best'}},
-  //     {Pizza: {type: String, ref: 'Best'}},
-  //     {Ramen: {type: String, ref: 'Best'}},
-  //     {Sushi: {type: String, ref: 'Best'}},
-  //     {Tacos: {type: String, ref: 'Best'}}
-  // ]
+  // bests: [{type: Number, ref: 'Best'}]
+  bests: [
+      {Bagels: {type: String, ref: 'Best'}},
+      {Brunch: {type: String, ref: 'Best'}},
+      {Burger: {type: String, ref: 'Best'}},
+      {Coffee: {type: String, ref: 'Best'}},
+      {Dessert: {type: String, ref: 'Best'}},
+      {Doughnuts: {type: String, ref: 'Best'}},
+      {IceCream: {type: String, ref: 'Best'}},
+      {Pizza: {type: String, ref: 'Best'}},
+      {Ramen: {type: String, ref: 'Best'}},
+      {Sushi: {type: String, ref: 'Best'}},
+      {Tacos: {type: String, ref: 'Best'}}
+  ]
   }
 });
 
